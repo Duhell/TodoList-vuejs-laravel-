aff403ec211e37e2b1d41182d6c555300ee9f37c
Duhell <ryanvillarma148@gmail.com>
2022-12-20 12:09:52 +0800

    C in crud

--- frontend/src/views/tasks/taskCreate.vue
+++ frontend/src/views/tasks/taskCreate.vue
@@ -1,7 +1,33 @@
 <script setup>
-	
+    import {reactive} from 'vue';
+    import useTasks from '../../composables/tasks';
+    const {storeTask,errors} = useTasks();
+    const form = reactive({
+        task: ""
+    });
 </script>
-
 <template>
-    <div>hello</div>
+  <form class="mt-5 w-3/5 mx-auto" @submit.prevent="storeTask(form)">
+    <div class="mb-6">
+      <label
+        for="task"
+        class="block mb-2 text-sm font-medium text-gray-900 dark:text-dark"
+        >Your Task</label
+      >
+      <input
+        type="text"
+        id="task"
+        v-model= "form.task"
+        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+        placeholder="My task for today..."
+        required
+      />
+    </div>
+    <input
+        type="submit"
+        id="create"
+        class="text-white bg-slate-700 hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-teal-800"
+        value="Create"
+      />
+  </form>
 </template>
